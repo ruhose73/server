1687546985d2dd2de644c200f908a41ecfc51449
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-07-15 00:09:09 +0300

    connect to MongoDB
    connect to MongoDB but we don't have any models
    
    TODO
    create models

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 5000
 
@@ -11,6 +12,11 @@ app.use(express.json())
 const start = async () => {
     
     try {
+        await mongoose.connect('mongodb+srv://ruhose73:chegevara3000@cluster0.7zmzo.mongodb.net/university_ecosystem?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        })
         app.listen(PORT, ()=>console.log('App has been started on port:', PORT))
 
     } catch (e) {
